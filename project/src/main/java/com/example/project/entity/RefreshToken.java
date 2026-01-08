ffa5d8049659292a80d9afebb7c9b727c478baae
Березовский Семён <ber2semen@gmail.com>
2026-01-08 14:03:22 +0700

    Update RefreshToken.java

--- project/src/main/java/com/example/project/entity/RefreshToken.java
+++ project/src/main/java/com/example/project/entity/RefreshToken.java
@@ -3,8 +3,6 @@ package com.example.project.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -13,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "refresh_tokens")
@@ -23,11 +20,8 @@ import lombok.RequiredArgsConstructor;
 @Builder
 public class RefreshToken {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    private   String token;
+    @Column(nullable = false, unique = true)
+    private String token;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
