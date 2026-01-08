df1ae9582cf1bb5e6ac4c1d2bbbce1b358291e02
Березовский Семён <ber2semen@gmail.com>
2026-01-08 13:50:01 +0700

    Update RefreshToken.java

--- project/src/main/java/com/example/project/entity/RefreshToken.java
+++ project/src/main/java/com/example/project/entity/RefreshToken.java
@@ -3,6 +3,8 @@ package com.example.project.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @Builder
 public class RefreshToken {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
